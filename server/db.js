dc3e7e7255ce2981562594cff51eeabca1ece406
despatch7 <indrajitmandal779@gmail.com>
2026-02-19 11:55:34 +0530

    feat: Initialize server with Express, MySQL database connection, and API route for STOs.

--- server/db.js
+++ server/db.js
@@ -2,10 +2,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'sto_tracker',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSAWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true
 });
 
